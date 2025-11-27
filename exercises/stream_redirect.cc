e2926d29fb6beb538e1f76ff684985ad9d2ee827
Till Kolditz <till.kolditz@gmail.com>
2025-11-27 14:37:56 +0100

    Extend stream redirection exercise with C-style FILE redirection
    * Note this is Linux-only, unsafe code!
    * If needed at all, you should most likely abstract from stdout/stderr
      and use a wrapper where you can set the FILE* -- or use modern C++!

--- exercises/stream_redirect.cc
+++ exercises/stream_redirect.cc
@@ -12,8 +12,50 @@
 
 using std::string_view_literals::operator""sv;
 
+int read_file_stream(std::filesystem::path const &path, std::fstream *fs) {
+  auto &stream = *fs;
+  stream.flush();
+  if (!stream) {
+    std::cout << "Error flushing file: " << std::strerror(errno) << "\n";
+    return errno;
+  }
+  stream.sync();
+  if (!stream) {
+    std::cout << "Error syncing file: " << std::strerror(errno) << "\n";
+    return errno;
+  }
+  stream.seekg(0, std::ios::beg);
+  if (!stream) {
+    std::cout << "Error seeking to file start: " << std::strerror(errno)
+              << "\n";
+    return errno;
+  }
+
+  size_t file_size = std::filesystem::file_size(path);
+  std::cout << "file size: " << file_size << '\n';
+  auto buf = std::array<char, 1024>{};
+  size_t total_gcount = 0;
+  while (stream && total_gcount < buf.size()) {
+    size_t read_size = buf.size() - total_gcount;
+    stream.read(buf.data() + total_gcount, read_size);
+    size_t gcount = stream.gcount();
+    std::cout << "read count: " << gcount << '\n';
+    total_gcount += gcount;
+  }
+  if (!stream && !stream.eof()) {
+    std::cout << "Error reading from file: " << std::strerror(errno)
+              << " fail=" << (stream.fail() ? "yes" : "no")
+              << " bad=" << (stream.bad() ? "yes" : "no") << "\n";
+    return errno;
+  }
+  auto view = std::string_view{buf.data(), total_gcount};
+  std::cout << "File contents:\n=====\n" << view << "\n=====\n";
+  return 0;
+}
+
 int main(int argc, char *argv[]) {
   {
+    std::cout << "Redirecting std::cerr to internal stringstream.\n";
     auto ss = std::stringstream{};
     auto redirect = stream_redirect{&std::cerr, &ss};
     std::cerr << "test error message1"sv;
@@ -21,8 +63,8 @@ int main(int argc, char *argv[]) {
   }
   {
     auto path = std::filesystem::path{argv[0]};
-    path += ".out";
-    std::cout << "Going to redirect std::cerr to '" << path << "'\n";
+    path += ".err1";
+    std::cout << "Redirecting std::cerr to '" << path << "'.\n";
     auto fs = std::fstream{path, std::ios::in | std::ios::out | std::ios::app |
                                      std::ios::ate};
     if (!fs) {
@@ -31,42 +73,34 @@ int main(int argc, char *argv[]) {
     }
     auto redirect = stream_redirect{&std::cerr, &fs};
     std::cerr << "test error message2\n"sv;
-    fs.flush();
-    if (!fs) {
-      std::cout << "Error flushing file: " << std::strerror(errno) << "\n";
-      return errno;
+    int err = read_file_stream(path, &fs);
+    if (err != 0) {
+      return err;
     }
-    fs.sync();
-    if (!fs) {
-      std::cout << "Error syncing file: " << std::strerror(errno) << "\n";
-      return errno;
-    }
-    fs.seekg(0, std::ios::beg);
-    if (!fs) {
-      std::cout << "Error seeking to file start: " << std::strerror(errno)
-                << "\n";
-      return errno;
-    }
-    size_t file_size = std::filesystem::file_size(path);
-    std::cout << "file size: " << file_size << '\n';
-    auto buf = std::array<char, 1024>{};
-    size_t total_gcount = 0;
-    while (fs && total_gcount < buf.size()) {
-      size_t read_size = buf.size() - total_gcount;
-      fs.read(buf.data() + total_gcount, read_size);
-      size_t gcount = fs.gcount();
-      std::cout << "read count: " << gcount << '\n';
-      total_gcount += gcount;
-    }
-    if (!fs && !fs.eof()) {
-      std::cout << "Error reading from file: " << std::strerror(errno)
-                << " fail=" << (fs.fail() ? "yes" : "no")
-                << " bad=" << (fs.bad() ? "yes" : "no") << "\n";
-      return errno;
-    }
-    auto view = std::string_view{buf.data(), total_gcount};
-    std::cout << "File contents:\n=====\n" << view << "\n=====\n";
   }
 
   std::cerr << "This should go to the default std::cerr stream again!\n";
+
+  {
+    auto path = std::filesystem::path{argv[0]};
+    path += ".err2";
+    std::cout << "Redirecting stderr to '" << path << "'.\n";
+
+    auto fr = file_redirect{stderr, path, std::ios::out | std::ios::trunc};
+    fprintf(stderr, "test error message3\n");
+    fr.flush();
+
+    auto fs = std::fstream{path, std::ios::in | std::ios::out | std::ios::app |
+                                     std::ios::ate};
+    if (!fs) {
+      std::cout << "Error opening file: " << std::strerror(errno) << "\n";
+      return errno;
+    }
+    int err = read_file_stream(path, &fs);
+    if (err != 0) {
+      return err;
+    }
+  }
+
+  fprintf(stderr, "This should go to the default stderr stream again!\n");
 }
--- exercises/stream_redirect.hpp
+++ exercises/stream_redirect.hpp
@@ -1,15 +1,21 @@
 #pragma once
 
+#include <cstdio>
+#include <fcntl.h>
+#include <filesystem>
+#include <ios>
 #include <ostream>
+#include <stdio.h>
 #include <streambuf>
+#include <string>
+#include <unistd.h>
 
 /**
- * Adapted from
- *
- * https://stackoverflow.com/questions/77633473/how-to-redirect-stderr-and-stdcerr-to-a-stringstream
+ * Adapted from https://stackoverflow.com/questions/77633473
  */
 class stream_redirect {
 public:
+  [[gnu::nonnull]]
   stream_redirect(std::ostream *old_stream, std::ostream *new_stream)
       : m_old_stream{old_stream}, m_old_streambuf{old_stream->rdbuf()},
         m_new_stream{new_stream} {
@@ -22,4 +28,115 @@ private:
   std::ostream *m_old_stream;
   std::streambuf *m_old_streambuf;
   std::ostream *m_new_stream;
+};
+
+/**
+ * Adapted from https://stackoverflow.com/questions/1908687
+ */
+class file_redirect {
+  struct mode {
+    bool read : 1;
+    bool write : 1;
+    bool append : 1;
+    bool at_end : 1;
+    bool truncate : 1;
+    // ignoring ios::binary for now
+  };
+
+public:
+  [[gnu::nonnull]]
+  file_redirect(FILE *old_file, std::filesystem::path new_file_path,
+                std::ios_base::openmode mode)
+      : m_old_file{old_file}, m_old_file_fd{dup(fileno(m_old_file))},
+        m_old_file_mode{fcntl64(m_old_file_fd, F_GETFL)},
+        m_new_file_path{new_file_path} {
+    parse_mode(mode);
+    auto *modes = make_new_mode_string();
+    m_new_file = std::freopen(m_new_file_path.c_str(), modes, m_old_file);
+  }
+
+  ~file_redirect() {
+    fclose(m_new_file);
+
+    const char *modes = make_old_mode_string();
+    fclose(m_old_file);
+    FILE *orig_file = fdopen(m_old_file_fd, modes);
+    if (move_old_to_end()) {
+      std::fseek(orig_file, 0, SEEK_END);
+    }
+    *m_old_file = *orig_file;
+  }
+
+  void flush() {
+    if (m_new_file != nullptr) {
+      fflush(m_new_file);
+    }
+  }
+
+private:
+  FILE *m_old_file{};
+  int m_old_file_fd{};
+  int m_old_file_mode{};
+  std::filesystem::path m_new_file_path;
+  FILE *m_new_file{};
+  mode m_new_file_mode{};
+
+  void parse_mode(std::ios_base::openmode mode) noexcept {
+    m_new_file_mode.read = (mode & std::ios::in) == std::ios::in;
+    m_new_file_mode.write = (mode & std::ios::out) == std::ios::out;
+    m_new_file_mode.append = (mode & std::ios::app) == std::ios::app;
+    m_new_file_mode.at_end = (mode & std::ios::ate) == std::ios::ate;
+    m_new_file_mode.truncate = (mode & std::ios::trunc) == std::ios::trunc;
+  }
+
+  /**
+   * Best-effort translation from std::ios_base::openmode to C fopen mode
+   * string.
+   */
+  const char *make_new_mode_string() const noexcept {
+    auto mode_str = std::string{};
+    if (m_new_file_mode.read && m_new_file_mode.write) {
+      if (m_new_file_mode.truncate) {
+        return "w+";
+      } else if (m_new_file_mode.append) {
+        return "a+";
+      } else {
+        return "r+";
+      }
+    } else if (m_new_file_mode.write) {
+      if (m_new_file_mode.truncate) {
+        return "w+";
+      } else if (m_new_file_mode.append) {
+        return "a+";
+      } else {
+        return "w";
+      }
+    } else if (m_new_file_mode.truncate) {
+      return "w+";
+    } else if (m_new_file_mode.read) {
+      return "r";
+    }
+    return "";
+  }
+
+  /**
+   * Best-effort translation from saved old file mode to C fopen mode string.
+   */
+  const char *make_old_mode_string() const noexcept {
+    if ((m_old_file_mode & O_RDWR) == O_RDWR) {
+      if ((m_old_file_mode & O_APPEND) == O_APPEND) {
+        return "a+";
+      } else {
+        return "r+";
+      }
+    } else if ((m_old_file_mode & O_WRONLY) == O_WRONLY) {
+      return "a";
+    } else {
+      return "r";
+    }
+  }
+
+  bool move_old_to_end() const noexcept {
+    return (m_old_file_mode & O_APPEND) == O_APPEND;
+  }
 };
