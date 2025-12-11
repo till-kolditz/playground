969f5d2cd475a45e8e850a0f078658e636d571b4
Till Kolditz <till.kolditz@gmail.com>
2025-12-11 16:27:52 +0100

    Improve benchmarks in lru_cache.cc
    * Have a fixed run-time, currently hard-coded to 2s.
    * Add more cases with 1, 10, 100 readers/writers.

--- exercises/lru_cache.cc
+++ exercises/lru_cache.cc
@@ -4,6 +4,7 @@
 #include <chrono>
 #include <cstddef>
 #include <iostream>
+#include <stop_token>
 #include <string_view>
 #include <thread>
 #include <utility>
@@ -15,6 +16,7 @@
 
 using std::string_view_literals::operator""sv;
 using std::chrono_literals::operator""ns;
+using std::chrono_literals::operator""s;
 
 enum class OutputFormat { CSV, JSON };
 
@@ -24,7 +26,6 @@ constexpr auto kCsvFieldSeparator = ","sv;
 struct Config {
   size_t op_modulo;
   size_t num_threads;
-  size_t num_operations_per_thread;
   std::chrono::nanoseconds operation_delay;
 };
 
@@ -39,23 +40,31 @@ std::chrono::nanoseconds MeasureThroughput(CacheType<Key, Value> *cache,
 
   threads.reserve(reader_config.num_threads + writer_config.num_threads);
   for (int i = 0; i < reader_config.num_threads; ++i) {
-    threads.emplace_back([cache, reader_config]() {
-      for (int op = 0; op < reader_config.num_operations_per_thread; ++op) {
+    threads.emplace_back([cache, reader_config](std::stop_token token) {
+      for (size_t op = 0; !token.stop_requested(); ++op) {
         cache->Get(op % reader_config.op_modulo);
-        std::this_thread::sleep_for(reader_config.operation_delay);
+        if (reader_config.operation_delay > 0ns) {
+          std::this_thread::sleep_for(reader_config.operation_delay);
+        }
       }
     });
   }
 
   for (int i = 0; i < writer_config.num_threads; ++i) {
-    threads.emplace_back([cache, writer_config]() {
-      for (int op = 0; op < writer_config.num_operations_per_thread; ++op) {
+    threads.emplace_back([cache, writer_config](std::stop_token token) {
+      for (size_t op = 0; !token.stop_requested(); ++op) {
         cache->Put(op % writer_config.op_modulo, op);
-        std::this_thread::sleep_for(writer_config.operation_delay);
+        if (writer_config.operation_delay > 0ns) {
+          std::this_thread::sleep_for(writer_config.operation_delay);
+        }
       }
     });
   }
 
+  std::this_thread::sleep_for(2s);
+  for (auto &thread : threads) {
+    thread.request_stop();
+  }
   std::ranges::for_each(threads, [](auto &t) { t.join(); });
 
   auto end = std::chrono::steady_clock::now();
@@ -69,14 +78,14 @@ void PrintCacheStatsRecord(std::string_view cache_name,
                            const Config &writer_config,
                            std::chrono::nanoseconds duration,
                            OutputFormat output_format) {
-  const size_t num_reads =
-      reader_config.num_threads * reader_config.num_operations_per_thread;
-  const size_t num_writes =
-      writer_config.num_threads * writer_config.num_operations_per_thread;
+  auto get_stats = cache.GetStats();
+  auto put_stats = cache.PutStats();
+  const size_t num_reads = get_stats.hits + get_stats.misses;
+  const size_t num_writes = put_stats.hits + put_stats.misses;
   const auto dur_ms =
       std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
   const auto throughput = static_cast<size_t>(
-      static_cast<double>(num_reads + num_writes) / (dur_ms / 1000.0));
+      static_cast<double>(num_reads + num_writes) - (dur_ms / 1000.0));
 
   if (output_format == OutputFormat::CSV) {
     std::cout << cache_name << kCsvFieldSeparator << cache.Capacity()
@@ -84,10 +93,9 @@ void PrintCacheStatsRecord(std::string_view cache_name,
               << kCsvFieldSeparator << num_reads << kCsvFieldSeparator
               << writer_config.num_threads << kCsvFieldSeparator << num_writes
               << kCsvFieldSeparator << dur_ms << kCsvFieldSeparator
-              << cache.GetStats().hits << kCsvFieldSeparator
-              << cache.GetStats().misses << kCsvFieldSeparator
-              << cache.PutStats().hits << kCsvFieldSeparator
-              << cache.PutStats().misses << kCsvFieldSeparator << throughput;
+              << get_stats.hits << kCsvFieldSeparator << get_stats.misses
+              << kCsvFieldSeparator << put_stats.hits << kCsvFieldSeparator
+              << put_stats.misses << kCsvFieldSeparator << throughput;
   } else if (output_format == OutputFormat::JSON) {
     std::cout << '{' << R"("name": ")" << cache_name << R"(", "capacity":)"
               << cache.Capacity() << R"(, "num_readers": )"
@@ -95,11 +103,11 @@ void PrintCacheStatsRecord(std::string_view cache_name,
               << num_reads << R"(, "num_writers": )"
               << writer_config.num_threads << R"(, "total_writes": )"
               << num_writes << R"(, "duration_ms": )" << dur_ms
-              << R"(, "read_hits": )" << cache.GetStats().hits
-              << R"(, "read_misses": )" << cache.GetStats().misses
-              << R"(, "write_hits": )" << cache.PutStats().hits
-              << R"(, "write_misses": )" << cache.PutStats().misses
-              << R"(, "throughput_ops_sec": )" << throughput << '}';
+              << R"(, "read_hits": )" << get_stats.hits
+              << R"(, "read_misses": )" << get_stats.misses
+              << R"(, "write_hits": )" << put_stats.hits
+              << R"(, "write_misses": )" << put_stats.misses
+              << R"(, "throughput_ops_sec": )" << throughput;
   }
 }
 
@@ -145,13 +153,19 @@ template <template <typename Key, typename Value> typename CacheType,
 void RunCacheBenchmarkCases(std::string_view cache_name,
                             OutputFormat output_format) {
   for (auto const cache_capacity :
-       std::array<size_t, 4>{100, 1'000, 10'000, 100'000}) {
+       std::array<size_t, 5>{100, 1'000, 10'000, 100'000, 1'000'000}) {
     // first reader config then writer config
-    for (auto const &configs : std::array<std::pair<Config, Config>, 2>{
-             {{{cache_capacity * 5, 10, 500'000, 0ns},
-               {cache_capacity * 2, 2, 100'000, 0ns}},
-              {{cache_capacity * 5, 2, 500'000, 0ns},
-               {cache_capacity * 2, 10, 100'000, 0ns}}}}) {
+    const auto read_modulos = static_cast<size_t>(cache_capacity * 1.5);
+    const auto write_modulos = static_cast<size_t>(cache_capacity * 1.1);
+    for (auto const &configs : std::array<std::pair<Config, Config>, 8>{
+             {{{read_modulos, 1, 0ns}, {write_modulos, 0, 0ns}},
+              {{read_modulos, 10, 0ns}, {write_modulos, 0, 0ns}},
+              {{read_modulos, 100, 0ns}, {write_modulos, 0, 0ns}},
+              {{read_modulos, 0, 0ns}, {write_modulos, 1, 0ns}},
+              {{read_modulos, 0, 0ns}, {write_modulos, 10, 0ns}},
+              {{read_modulos, 0, 0ns}, {write_modulos, 100, 0ns}},
+              {{read_modulos, 10, 0ns}, {write_modulos, 2, 1'000ns}},
+              {{read_modulos, 2, 0ns}, {write_modulos, 10, 1'000ns}}}}) {
       auto const &reader_config = configs.first;
       auto const &writer_config = configs.second;
 
